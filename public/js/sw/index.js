e1e91f0e462186428687709eb2dc982e0b6bc359
Zac Cogley <zaccogley@gmail.com>
2018-01-15 12:02:51 -0500

    complete 3.25

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -40,3 +40,8 @@ self.addEventListener('fetch', function(event) {
 
 // TODO: listen for the "message" event, and call
 // skipWaiting if you get the appropriate message
+self.addEventListener('message', function (event) {
+  if (event.data.action == 'skipWaiting') {
+    self.skipWaiting();
+  }
+})
