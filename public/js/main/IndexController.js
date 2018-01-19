0bc937a05a6a1f6433c7ef372f507b426ac44ce9
Zac Cogley <zaccogley@gmail.com>
2018-01-19 11:22:01 -0500

    4.7 complete; pulling messages from cache when offline

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -82,6 +82,14 @@ IndexController.prototype._showCachedMessages = function() {
     // in order of date, starting with the latest.
     // Remember to return a promise that does all this,
     // so the websocket isn't opened until you're done!
+    var tx = db.transaction('wittrs');
+    var store = tx.objectStore('wittrs');
+    var index = store.index('by-date');
+
+    return index.getAll().then(function(messages) {
+      indexController._postsView.addPosts(messages.reverse());
+    });
+
   });
 };
 
